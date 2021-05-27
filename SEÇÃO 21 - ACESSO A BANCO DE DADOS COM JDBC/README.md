51d5de1575a3140ade9cd27406e3936df213c67f
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-05-27 09:43:57 -0300

    Seção 21 - propriedades e conexão com o banco

---
+++ SEÇÃO 21 - ACESSO A BANCO DE DADOS COM JDBC/README.md
@@ -0,0 +1,9 @@
+<h1>Seção 21 - Acesso ao banco de dados usando JDBC</h1>
+<ol>
+<li>Criar a base de dados</li>
+<li>Baixar MySql java connector</li>
+<li>criar em c: uma pasta para guardar bibliotecas externas</li>
+<li>adicionar biblioteca no eclipse window -> preferences -> java -> build path -> user libraries</li>
+<li>acresentar a biblioteca no projeto java. </li>
+<li>criar arquivo db.properties</li>
+</ol>
---
+++ SEÇÃO 21 - ACESSO A BANCO DE DADOS COM JDBC/src/db/DB.java
@@ -0,0 +1,48 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+	// Connection usado para conexão com banco de dados
+	private static Connection conn = null;
+
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	// função para pegas os dados de conexão salvo no arquivo properties.
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);// faz a leitura do arquivo e guarda os dados dentro do objeto properties.
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+}

6a5c6350d11434801b71aefb5c1b4196f064be20
Márcio Gabriel Mengali <marciogabriel1998@gmail.com>
2021-05-30 11:20:22 -0300

    Seller entitie Class

--- SEÇÃO 21 - PROJETO - DAO/src/application/Program.java
+++ SEÇÃO 21 - PROJETO - DAO/src/application/Program.java
@@ -1,11 +1,11 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.Date;
 
 import db.DB;
-import db.DbException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -13,6 +13,8 @@ public class Program {
 		Connection conn = null;
 		Department obj = new Department(1 , "Books");
 		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		System.out.println(seller);
 		try {
 			conn = DB.getConnection();
 		}finally {

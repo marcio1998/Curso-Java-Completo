b87e9349ef98d6481e84c0475253d92ea3e6bb9e
Márcio Gabriel Mengali <marciogabriel1998@gmail.com>
2021-05-30 11:11:39 -0300

    Departmente entitie Class

--- SEÇÃO 21 - PROJETO - DAO/src/application/Program.java
+++ SEÇÃO 21 - PROJETO - DAO/src/application/Program.java
@@ -5,11 +5,14 @@ import java.sql.SQLException;
 
 import db.DB;
 import db.DbException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Connection conn = null;
+		Department obj = new Department(1 , "Books");
+		System.out.println(obj);
 		try {
 			conn = DB.getConnection();
 		}finally {

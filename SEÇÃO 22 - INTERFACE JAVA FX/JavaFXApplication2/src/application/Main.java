4b263c3d895e9acdbd10e30442496a9b0788ebb9
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-06-01 16:51:24 -0300

    Seção 22 - Completa

---
+++ SEÇÃO 22 - INTERFACE JAVA FX/JavaFXApplication2/src/application/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Main extends Application {
+   @Override
+   public void start(Stage stage){
+       try{
+           Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+           Scene scene = new Scene(parent);
+           stage.setScene(scene);
+           stage.show();
+       }catch(IOException e){
+           e.printStackTrace();
+       }
+   }
+   
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ SEÇÃO 22 - INTERFACE JAVA FX/README.md
@@ -0,0 +1,2 @@
+<h1>Interface Com JavaFx</h1>
+<p>Instalar Scene Builder</p>

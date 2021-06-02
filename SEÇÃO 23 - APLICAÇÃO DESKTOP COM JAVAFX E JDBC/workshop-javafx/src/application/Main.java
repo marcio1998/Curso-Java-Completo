e4535699c72012811a43a8ddbd21444c3f5bbf59
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-06-02 14:22:24 -0300

    MainViewController

--- SEÇÃO 23 - APLICAÇÃO DESKTOP COM JAVAFX E JDBC/workshop-javafx/src/application/Main.java
+++ SEÇÃO 23 - APLICAÇÃO DESKTOP COM JAVAFX E JDBC/workshop-javafx/src/application/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
         @Override
         public void start(Stage primaryStage){
             try{
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/View.fxml"));
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
                 ScrollPane scrollPane = loader.load();
                 scrollPane.setFitToHeight(true);
                 scrollPane.setFitToWidth(true);
---
+++ SEÇÃO 23 - APLICAÇÃO DESKTOP COM JAVAFX E JDBC/workshop-javafx/src/gui/MainViewController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class MainViewController implements Initializable{
+    @FXML
+    private MenuItem menuItemSeller;
+    
+    @FXML
+    private MenuItem menuItemDepartment;
+    
+    @FXML
+    private MenuItem menuItemAbout;
+    
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+    
+    @FXML
+    public void onMenuItemDepartmentAction(){
+        System.out.println("onMenuItemDepartmenAction");
+    }
+    
+    @FXML
+    public void onMenuItemAboutAction(){
+        System.out.println("onMenuItemAboutAction");
+    }
+    
+    
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+  
+    }
+    
+}

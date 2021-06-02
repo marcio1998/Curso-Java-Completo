17b0c0d753acdf03bfb21f4a8ecae3bfc9e31a40
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-06-02 13:53:50 -0300

    Configuração Inicial do Projeto Definições de Bibliotecas

---
+++ SEÇÃO 23 - APLICAÇÃO DESKTOP COM JAVAFX E JDBC/workshop-javafx/src/application/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Main extends Application {
+        @Override
+        public void start(Stage primaryStage){
+            try{
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/View.fxml"));
+                Parent parent = loader.load();
+                Scene mainScene = new Scene(parent);
+                primaryStage.setScene(mainScene);
+                primaryStage.setTitle("Sample JavaFx application");
+                primaryStage.show();
+                
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        public static void main(String[] args) {
+        launch(args);
+    }
+    
+}

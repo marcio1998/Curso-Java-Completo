7ae8edecc559217c72ca7565364f94ff4879906b
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-06-07 14:24:44 -0300

    Remove Department

--- SEÇÃO 23 - APLICAÇÃO DESKTOP COM JAVAFX E JDBC/workshop-javafx/src/gui/DepartmentListController.java
+++ SEÇÃO 23 - APLICAÇÃO DESKTOP COM JAVAFX E JDBC/workshop-javafx/src/gui/DepartmentListController.java
@@ -1,12 +1,14 @@
 package gui;
 
 import application.Main;
+import db.DbException;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
@@ -18,6 +20,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -43,6 +46,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
     @FXML
     private TableColumn<Department, Department> tableColumnEdit;
+
+    @FXML
+    private TableColumn<Department, Department> tableColumnRemove;
 
     @FXML
     private Button btNew;
@@ -81,6 +87,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
         obsList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(obsList);
         initEditButtons();
+        initRemoveButtons();
     }
 
     private void CreateDialogForm(Department obj, String absolutName, Stage parentStage) {
@@ -127,6 +134,39 @@ public class DepartmentListController implements Initializable, DataChangeListen
                                 obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));
             }
         });
+    }
+
+    private void initRemoveButtons() {
+        tableColumnRemove.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnRemove.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("remove");
+
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(event -> removeEntity(obj));
+            }
+        });
+    }
+    
+    private void removeEntity(Department obj){
+        Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete ?");
+        if(result.get() == ButtonType.OK){
+            if(service == null){
+                throw new IllegalStateException("Service was null");
+            }
+            try{
+                service.remove(obj);
+                updateTableView();
+            }catch(DbException e){
+                Alerts.showAlert("Error removing onject", null, e.getMessage(), Alert.AlertType.ERROR);
+            }
+        }
     }
 
 }

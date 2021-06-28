c998d9e7b92d3d423a4aedef673c97fd061a92b4
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-06-28 17:53:46 -0300

    Referenciado os posts do usuário

--- SEÇÃO 27 - API RESTFUL COM SPRINGBOOT E MONGODB/workshopmongo/src/main/java/com/example/demo/config/Instantiation.java
+++ SEÇÃO 27 - API RESTFUL COM SPRINGBOOT E MONGODB/workshopmongo/src/main/java/com/example/demo/config/Instantiation.java
@@ -35,6 +35,8 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu Viagem!", "Vou viajar para são Paulo, abraços!!",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom Dia!", "Acordei Feliz Hoje", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 		
 		
 	}

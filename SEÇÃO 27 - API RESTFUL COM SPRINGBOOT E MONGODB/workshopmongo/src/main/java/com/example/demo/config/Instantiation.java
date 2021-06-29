7eb660e8a90abb13aafc7f7178811002b430b99e
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-06-29 10:50:13 -0300

    Adicionando comentários aos posts

--- SEÇÃO 27 - API RESTFUL COM SPRINGBOOT E MONGODB/workshopmongo/src/main/java/com/example/demo/config/Instantiation.java
+++ SEÇÃO 27 - API RESTFUL COM SPRINGBOOT E MONGODB/workshopmongo/src/main/java/com/example/demo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
 import com.example.demo.dto.AuthorDTO;
+import com.example.demo.dto.CommentDTO;
 import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.UserRepository;
 
@@ -34,6 +35,11 @@ public class Instantiation implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu Viagem!", "Vou viajar para são Paulo, abraços!!",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom Dia!", "Acordei Feliz Hoje", new AuthorDTO(maria));
+		CommentDTO c1 = new CommentDTO("Boa Viagem Mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);

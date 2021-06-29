b2339bd3f78d6602c34e03fc27749dd001d5f57c
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-06-29 11:39:15 -0300

    Projeto Finalizado

--- SEÇÃO 27 - API RESTFUL COM SPRINGBOOT E MONGODB/workshopmongo/src/main/java/com/example/demo/services/PostService.java
+++ SEÇÃO 27 - API RESTFUL COM SPRINGBOOT E MONGODB/workshopmongo/src/main/java/com/example/demo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,11 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return postRepository.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 *60 *1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 	
 
